1504a3fdda9c053e3079d8ee74507b37f062080f
qxtang <qx.tang@foxmail.com>
2023-07-12 17:02:50 +0800

    feat: 适配手机

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.__utils = {
-  loadCSS: function (src) {
+  loadCSS: function(src) {
     var head = document.getElementsByTagName('head')[0];
     var link = document.createElement('link');
     link.rel = 'stylesheet';
@@ -9,7 +9,7 @@ window.__utils = {
     head.appendChild(link);
   },
 
-  getIsMobile: function () {
+  getIsMobile: function() {
     return !!(
       navigator.userAgent.match(/Android/i) ||
       navigator.userAgent.match(/webOS/i) ||
@@ -24,11 +24,11 @@ window.__utils = {
   getBoardMainScale() {
     const originWidth = 590;
     const vw = document.body.clientWidth / 100;
-    return ((70 * vw) / originWidth).toFixed(2);
+    return ((74 * vw) / originWidth).toFixed(2);
   }
 };
 
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function() {
   const isMobile = window.__utils.getIsMobile();
 
   if (isMobile) {
@@ -40,13 +40,21 @@ window.addEventListener('DOMContentLoaded', function () {
 
     const boardScale = window.__utils.getBoardMainScale();
     boardEle.css('zoom', boardScale.toString());
-
     boardWrapEle.after(playControlEle);
+
+    const controlbarBtnsEle = playControlEle.children('.buttons').children();
+
+    setTimeout(() => {
+      const controlbarEle = $('.controlbar');
+      console.log('uuuu', controlbarEle);
+
+      controlbarEle.append(controlbarBtnsEle);
+    }, 0);
   }
 
   let board = null;
   let record_player = null;
-  let boardObj = function () {
+  let boardObj = function() {
     // 棋盘的DOM对象，基本上棋子、棋盘逻辑都在这里面。
     let board = $('#board_main');
 
@@ -72,7 +80,7 @@ window.addEventListener('DOMContentLoaded', function () {
     _obj.curr_step = 1;
 
     // 标记是不是zj
-    _obj.setZj = function (row) {
+    _obj.setZj = function(row) {
       let action = row['action'];
       if (action == 'RESET' || action == 'LOAD' || action == 'MOVE') {
         if (row['content'].length >= 2 && row['content'].substr(0, 2) == 'ZJ') {
@@ -84,7 +92,7 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     //load 一个游戏数据。
-    _obj.load = function (game_record) {
+    _obj.load = function(game_record) {
       //为了播放声音，这里对比一下旧盘面和新load的盘面，决定是否播放一次声音
       //let play_sound = (_obj.currgame != game_data.game_record);
       //_obj.game_record = game_record;
@@ -97,9 +105,9 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     //自动切换模式。
-    _obj.switch_mode = (function () {
+    _obj.switch_mode = (function() {
       let _mode = 'game'; // game or analyze
-      return function (mode) {
+      return function(mode) {
         if (mode == _mode) {
           return true;
         }
@@ -123,7 +131,7 @@ window.addEventListener('DOMContentLoaded', function () {
      * @param  {string} coordinate 传入坐标。
      * @returns {boolean}
      */
-    _obj.place_mark = function (mark_obj, is_add) {
+    _obj.place_mark = function(mark_obj, is_add) {
       let target_cell = board.find('.' + mark_obj.coordinate);
       if (!target_cell.hasClass('blank')) {
         return false;
@@ -143,7 +151,7 @@ window.addEventListener('DOMContentLoaded', function () {
       return true;
     };
 
-    _obj.place_mark_step = function () {
+    _obj.place_mark_step = function() {
       if (!_obj.mark.hasOwnProperty(_obj.curr_step)) {
         return;
       }
@@ -156,7 +164,7 @@ window.addEventListener('DOMContentLoaded', function () {
       return true;
     };
 
-    _obj.clear_mark = function () {
+    _obj.clear_mark = function() {
       for (var key in _obj.mark) {
         let mark_obj = _obj.mark[key];
         for (let i = 0; i < mark_obj.length; i++) {
@@ -176,7 +184,7 @@ window.addEventListener('DOMContentLoaded', function () {
      * @param  {boolean} play_sound 是否播放声音
      * @returns {boolean}
      */
-    _obj.place_stone = function (coordinate, play_sound) {
+    _obj.place_stone = function(coordinate, play_sound) {
       black = null;
       if (_obj.zj == true) {
         black = coordinate.charAt(0) == '0' ? 'black' : 'white';
@@ -219,7 +227,7 @@ window.addEventListener('DOMContentLoaded', function () {
       return true;
     };
 
-    _obj.get_current_board = function () {
+    _obj.get_current_board = function() {
       return _obj.currgame;
     };
 
@@ -227,7 +235,7 @@ window.addEventListener('DOMContentLoaded', function () {
      * 右键和回退按钮的事件，往回退一个棋子。并不产生任何Ajax，这不是悔棋操作。
      * @returns {boolean}
      */
-    _obj.move_pre = function () {
+    _obj.move_pre = function() {
       if (_obj.currgame) {
         let len = 2;
         if (_obj.zj == true) {
@@ -256,7 +264,7 @@ window.addEventListener('DOMContentLoaded', function () {
      * 根据endgame，一步一步走下去，把整个棋局展示出来。
      * @returns {boolean}
      */
-    _obj.move_next = function () {
+    _obj.move_next = function() {
       if (_obj.currgame != _obj.endgame) {
         let nextstep = _obj.endgame.substr(
           _obj.currgame.length,
@@ -271,21 +279,21 @@ window.addEventListener('DOMContentLoaded', function () {
     /**
      * 回退到空棋盘状态。
      */
-    _obj.board_clean = function () {
+    _obj.board_clean = function() {
       while (_obj.move_pre()) {}
     };
 
     /**
      * 根据目前的棋局记录一路Next到局面结束的状态。
      */
-    _obj.board_end = function () {
+    _obj.board_end = function() {
       while (_obj.move_next()) {}
     };
 
     /**
      * 根据game_record 初始化棋盘的文字信息和棋盘Game信息
      */
-    _obj.show_origin = function () {
+    _obj.show_origin = function() {
       //_obj.switch_mode('game');
       _obj.board_clean();
       //_obj.endgame = _obj.game_record;
@@ -296,13 +304,13 @@ window.addEventListener('DOMContentLoaded', function () {
      * 画棋盘和按钮。绑定右键事件。
      * 整个页面载入的时候会执行一次。仅此一次。
      */
-    _obj.init_board = function () {
+    _obj.init_board = function() {
       _obj.currgame = '';
       _obj.curr_color = 'white';
       _obj.curr_step = 1;
       board.html('');
 
-      board.bind('contextmenu', function () {
+      board.bind('contextmenu', function() {
         return false;
       });
       for (let i = 15; i >= 1; i--) {
@@ -319,7 +327,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
         board.append(newrow);
       }
-      board.find('.row div').click(function () {
+      board.find('.row div').click(function() {
         let coordinate = $(this).attr('alt');
         let c_x = parseInt(coordinate.charAt(0), 16) + 96;
         let c_y = parseInt(coordinate.charAt(1), 16);
@@ -424,7 +432,7 @@ window.addEventListener('DOMContentLoaded', function () {
       $(document.createElement('button'))
         .addClass('button')
         .text('计算分析')
-        .click(function () {
+        .click(function() {
           record_player.pause();
 
           const gameBaseUrl = '/ku10/gomoku-calculator/#/';
@@ -441,7 +449,7 @@ window.addEventListener('DOMContentLoaded', function () {
       $(document.createElement('button'))
         .addClass('button show')
         .text('隐藏数字')
-        .click(function () {
+        .click(function() {
           let _btn = $(this);
           if (_btn.hasClass('show')) {
             _btn.text('显示数字').removeClass('show');
@@ -457,19 +465,18 @@ window.addEventListener('DOMContentLoaded', function () {
         .appendTo(controlbar);
 
       // 创建下拉选择
-      (function () {
+      (function() {
         const options = [
           { val: 1, text: 'RIF' },
           { val: 25, text: 'Taraguchi-10' }
         ];
 
         const selectEle = $('<select id="query-actual-combat-chess">');
-        $(options).each(function () {
+        $(options).each(function() {
           selectEle.append(
             $('<option>').attr('value', this.val).text(this.text)
           );
         });
-        selectEle.css({ margin: '0 5px' });
         selectEle.appendTo(controlbar);
       })();
 
@@ -477,7 +484,7 @@ window.addEventListener('DOMContentLoaded', function () {
       $(document.createElement('button'))
         .addClass('button')
         .text('查实战棋谱')
-        .click(function () {
+        .click(function() {
           record_player.pause();
           const val = $('#query-actual-combat-chess').val();
           const url = `https://www.renju.net/game/search?moves=${parsingCurrgame(
@@ -494,7 +501,7 @@ window.addEventListener('DOMContentLoaded', function () {
     };
   };
 
-  let recordPlayer = function (boardHandler) {
+  let recordPlayer = function(boardHandler) {
     let obj = this;
     this.pointer = 0; //当前播放到第几个action
     this.timer = 0; //计时器
@@ -502,7 +509,7 @@ window.addEventListener('DOMContentLoaded', function () {
     this.status = 0;
     this.boardHandler = boardHandler;
     this._progress = -1;
-    obj.load = function (record) {
+    obj.load = function(record) {
       obj.pause();
       obj.record = record;
       obj.pointer = 0;
@@ -514,7 +521,7 @@ window.addEventListener('DOMContentLoaded', function () {
       $('#progress>div').css('width', '0%');
       obj.play();
     };
-    obj.play = function () {
+    obj.play = function() {
       console.log(obj._progress);
       if (typeof obj.record[obj.pointer] == 'undefined') {
         return false;
@@ -546,14 +553,14 @@ window.addEventListener('DOMContentLoaded', function () {
         let speed = parseInt($('select[name=speed]').val());
         //progress快进逻辑
         _timeout = obj._progress == -1 ? 10 + speed * delta_time : 3;
-        obj.timer = setTimeout(function () {
+        obj.timer = setTimeout(function() {
           obj.play();
         }, _timeout);
       } else {
         obj.sysMsg('播放完毕');
       }
     };
-    obj.pause = function () {
+    obj.pause = function() {
       if (obj.timer) {
         obj.status = 0;
         clearTimeout(obj.timer);
@@ -561,7 +568,7 @@ window.addEventListener('DOMContentLoaded', function () {
         obj.sysMsg('播放暂停');
       }
     };
-    obj.gotoProgress = function (i) {
+    obj.gotoProgress = function(i) {
       if (i < 0 || i > 99) {
         return;
       }
@@ -577,7 +584,7 @@ window.addEventListener('DOMContentLoaded', function () {
       obj.play();
     };
 
-    obj.renderRow = function (row) {
+    obj.renderRow = function(row) {
       //console.log(row);
       this.boardHandler.setZj(row);
       let function_name = 'action' + row['action'];
@@ -594,7 +601,7 @@ window.addEventListener('DOMContentLoaded', function () {
       }
       $('#chat_content_list').scrollTop(
         $('#chat_content_list')[0].scrollHeight -
-          $('#chat_content_list').height()
+        $('#chat_content_list').height()
       );
       return $return;
     };
@@ -602,7 +609,7 @@ window.addEventListener('DOMContentLoaded', function () {
     /**
      * 以下是各种事件的处理方法。
      */
-    obj.actionMARK = function (row) {
+    obj.actionMARK = function(row) {
       start = 2;
       for (i = 0; i < row.content.length - 2; i += 3) {
         let c_w = row.content.charAt(start + i + 2);
@@ -617,12 +624,12 @@ window.addEventListener('DOMContentLoaded', function () {
       return true;
     };
 
-    obj.actionRESET = function () {
+    obj.actionRESET = function() {
       this.boardHandler.load('');
       return true;
     };
 
-    obj.actionTALK = function (row) {
+    obj.actionTALK = function(row) {
       let _name = $('<span>').html(row.user).addClass('chat-name');
       let _content = $('<span>').html(row.content).addClass('chat-content');
       if (row.user == 'RenjuTeacher' || row.user == 'GrandMaster') {
@@ -633,7 +640,7 @@ window.addEventListener('DOMContentLoaded', function () {
       li.appendTo($('#chat_content'));
       return true;
     };
-    obj.actionMOVE = function (row) {
+    obj.actionMOVE = function(row) {
       start = 0;
       if (this.boardHandler.zj) {
         start = 1;
@@ -657,35 +664,35 @@ window.addEventListener('DOMContentLoaded', function () {
 
       obj.sysMsg(
         row.user +
-          ' - ' +
-          (this.boardHandler.curr_color == 'black' ? '黑' : '白') +
-          obj.getPoint(row.content.substr(start, 2))
+        ' - ' +
+        (this.boardHandler.curr_color == 'black' ? '黑' : '白') +
+        obj.getPoint(row.content.substr(start, 2))
       );
 
       return true;
     };
 
-    obj.actionCHECKRESULT = function (row) {
+    obj.actionCHECKRESULT = function(row) {
       obj.sysMsg('点名结束，以下学生答到：' + row.content);
       return true;
     };
 
-    obj.actionEMOTE = function (row) {
+    obj.actionEMOTE = function(row) {
       return obj.actionTALK(row);
     };
 
-    obj.actionGOTO = function (row) {
+    obj.actionGOTO = function(row) {
       this.boardHandler.clear_mark();
       while (
         this.boardHandler.get_current_board().length >
         row.content * (this.boardHandler.zj ? 3 : 2)
-      ) {
+        ) {
         if (!this.boardHandler.move_pre()) break;
       }
       while (
         this.boardHandler.get_current_board().length <
         row.content * (this.boardHandler.zj ? 3 : 2)
-      ) {
+        ) {
         if (!this.boardHandler.move_next()) break;
       }
       this.boardHandler.place_mark_step();
@@ -693,7 +700,7 @@ window.addEventListener('DOMContentLoaded', function () {
       return true;
     };
 
-    obj.actionBACK = function (row) {
+    obj.actionBACK = function(row) {
       obj.sysMsg(row.user + ' - 回退');
       if (this.boardHandler.zj && this.boardHandler.curr_step <= 2) {
         return true;
@@ -705,7 +712,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
       return true;
     };
-    obj.actionFIRST = function (row) {
+    obj.actionFIRST = function(row) {
       this.boardHandler.clear_mark();
       this.boardHandler.board_clean();
       this.boardHandler.move_next();
@@ -713,24 +720,24 @@ window.addEventListener('DOMContentLoaded', function () {
       obj.sysMsg(row.user + ' - 回第一手');
       return true;
     };
-    obj.actionCREDIT = function (row) {
+    obj.actionCREDIT = function(row) {
       obj.sysMsg('本次课程拿到学分的同学：' + row.content);
       return true;
     };
-    obj.actionNEXT = function (row) {
+    obj.actionNEXT = function(row) {
       this.boardHandler.clear_mark();
       this.boardHandler.move_next();
       this.boardHandler.place_mark_step();
       obj.sysMsg(row.user + ' - 前进');
       return true;
     };
-    obj.actionCLEAR = function (row) {
+    obj.actionCLEAR = function(row) {
       this.boardHandler.clear_mark();
       this.boardHandler.load('');
       obj.sysMsg(row.user + ' - 清空棋盘');
       return true;
     };
-    obj.actionLOAD = function (row) {
+    obj.actionLOAD = function(row) {
       this.boardHandler.clear_mark();
       this.boardHandler.load(row.content);
       obj.sysMsg(
@@ -738,14 +745,14 @@ window.addEventListener('DOMContentLoaded', function () {
       );
       return true;
     };
-    obj.actionLAST = function (row) {
+    obj.actionLAST = function(row) {
       this.boardHandler.clear_mark();
       this.boardHandler.board_end();
       this.boardHandler.place_mark_step();
       obj.sysMsg(row.user + ' - 到最后一手');
       return true;
     };
-    obj.getPoint = function (coordinate) {
+    obj.getPoint = function(coordinate) {
       // start =0
       // if(this.boardHandler.zj){
       //     start = 1
@@ -761,13 +768,13 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     //事件处理结束
-    obj.sysMsg = function (msg) {
+    obj.sysMsg = function(msg) {
       let li = $('<li>');
       $('<span>').addClass('chat-sys').html(msg).appendTo(li);
       li.appendTo($('#chat_content'));
       $('#chat_content_list').scrollTop(
         $('#chat_content_list')[0].scrollHeight -
-          $('#chat_content_list').height()
+        $('#chat_content_list').height()
       );
     };
   };
@@ -775,16 +782,16 @@ window.addEventListener('DOMContentLoaded', function () {
   //1.new出对象
   board = new boardObj();
   record_player = new recordPlayer(board);
-  $(document).ready(function () {
+  $(document).ready(function() {
     board.init_board();
-    $('#progress>ul>li').each(function (idx) {
-      $(this).click(function () {
+    $('#progress>ul>li').each(function(idx) {
+      $(this).click(function() {
         record_player.gotoProgress(idx + 1);
         //console.log(idx+1)
       });
     });
-    let load_game = function (source, callback) {
-      $.getJSON('json/' + source, {}, function (_data) {
+    let load_game = function(source, callback) {
+      $.getJSON('json/' + source, {}, function(_data) {
         //play data
         //console.log(source);
         record_player.load(_data);
@@ -793,7 +800,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
       });
     };
-    let auto_play = function () {
+    let auto_play = function() {
       console.log(window.location.hash);
       if (window.location.hash.charAt(0) == '#') {
         console.log(window.location.hash.charAt(0));
@@ -809,12 +816,12 @@ window.addEventListener('DOMContentLoaded', function () {
             .click();
         }
       } else {
-        load_game('20181229.json', function () {
+        load_game('20181229.json', function() {
           //record_player.sysMsg("欢迎！");
         });
       }
     };
-    $.getJSON('trans.json?d=20190104', {}, function (_data) {
+    $.getJSON('trans.json?d=20190104', {}, function(_data) {
       for (let i in _data) {
         let _tmp = _data[i];
         let _link = $(document.createElement('a'));
@@ -824,11 +831,11 @@ window.addEventListener('DOMContentLoaded', function () {
           .attr('href', '#' + i)
           .attr('id', 'play_link_' + i)
           .html(_tmp['show_name']);
-        _link.click(function () {
+        _link.click(function() {
           $('title').text($(this).text() + ' @ 连珠课程在线学习');
           record_player.sysMsg('Loading......');
           //loadCounter();
-          load_game($(this).attr('data-source'), function () {
+          load_game($(this).attr('data-source'), function() {
             record_player.sysMsg('开始播放' + $('title').text());
           });
         });
@@ -840,13 +847,13 @@ window.addEventListener('DOMContentLoaded', function () {
       }
       auto_play();
     });
-    $('.btn_play').click(function () {
+    $('.btn_play').click(function() {
       if (record_player.status == 0) {
         record_player.sysMsg('继续播放');
         record_player.play();
       }
     });
-    $('.btn_pause').click(function () {
+    $('.btn_pause').click(function() {
       if (record_player.status == 1) {
         record_player.pause();
       }
